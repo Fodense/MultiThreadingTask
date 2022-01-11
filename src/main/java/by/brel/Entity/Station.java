903a95481055c78d46b3e87c7232f0d3c197ec47
Vladislav Brel <vlad.brel.97@mail.ru>
2022-01-11 17:59:11 +0300

    Created the basic logic

---
+++ src/main/java/by/brel/Entity/Station.java
@@ -0,0 +1,69 @@
+package Entity;
+
+import org.apache.log4j.Logger;
+
+public class Station {
+
+    private static final Logger log = Logger.getLogger(Station.class);
+    private Bus bus;
+
+    private int numberStation;
+    private int countPassenger;
+
+    public Station() {
+    }
+
+    public Station(int numberStation) {
+        this.numberStation = numberStation;
+    }
+
+    public synchronized Bus passengerWaitBus(int numberStation) {
+        this.countPassenger++;
+        boolean flag = true;
+
+        try {
+            while (flag) {
+                log.info("Пассажир " + Thread.currentThread().getName() + " прибыл на остановку и ожидает автобус");
+                this.wait();
+
+                if (bus.getCapacityBus() > 0 && bus.getMovementInterval() == numberStation) {
+                    bus.addPassenger();
+                    bus.notifyBus();
+
+                    this.countPassenger--;
+
+                    flag = false;
+
+                    log.info("Пассажир сел в автобус");
+
+                } else {
+                    log.info("Мест в автобусе " + bus + " нет");
+
+                    this.wait();
+                }
+
+                return bus;
+            }
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public void busWaitPassenger(Bus bus) {
+        synchronized (this) {
+
+            bus.setStation(this);
+
+            if (bus.getCountPassenger() != 0) {
+                bus.notifyAllPassenger();
+                bus.waitBus();
+            }
+
+            this.bus = bus;
+            this.notifyAll();
+        }
+    }
+}
